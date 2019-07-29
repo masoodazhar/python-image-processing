b5435459469415422817d9659a2958a3a150e8a7
masoodazhar <masoodazhar60@yahoo.com>
2019-07-29 14:27:43 +0500

    complete reading image
    first importing cv2. installed from (  pip install opencv-python ) command
    2) created a class with constructor which take a parameter as imagePath
    3) image will be read by cv2.imread(imagePath)
    4)resizing the image size as 300x300
        and creating a circle of (250x250)
        and creating a rectangle of (50x50) 
        at the end showing the image    using cv2.imshow("frame name", image)
    5) last waiting for a key pressed by user then destroy all windows and frames

---
+++ image.py
@@ -0,0 +1,30 @@
+import cv2 #''' importing opencv '''
+
+class ImageProccess():
+    
+    #contructor method take an argument for image path like.
+    # while creating object of ImageProccess('image.jpg')
+    def __init__(self, imgageUrl):
+        #reading the image from current folder
+        img = cv2.imread(imgageUrl) 
+
+        #resizing the image and replacing (img) variable
+        img = cv2.resize(img, (300,300))  
+
+        # creating rectangle in img and replacing img again
+        img = cv2.circle(img,(150,150),100, (0,0,255),3) 
+       
+        # creating circle on image and replacing img again
+        img = cv2.rectangle(img, (0,300), (300, 250),(0,0,255),-1 ) 
+
+        # showing the img in frame named (image)
+        cv2.imshow("image", img) 
+
+        # the frame wait until user press any key
+        cv2.waitKey(0)     
+
+        #destroying AllWindows and frames after pressing any key     
+        cv2.destroyAllWindows()  
+
+
+object = ImageProccess('b.jpg')
